30b318c3f0a3d1939546f2d71dc9b1c971763398
lisiant <wkdxod321@gmail.com>
2023-09-05 20:22:59 +0900

    요구사항 5 로그인 직전까지 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -5,11 +5,16 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.Map;
 
+import db.DataBase;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import util.HttpRequestUtils;
+import util.IOUtils;
+
+import static util.HttpRequestUtils.*;
+import static util.HttpRequestUtils.parseQueryString;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -30,6 +35,8 @@ public class RequestHandler extends Thread {
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             StringBuilder sb = new StringBuilder();
             String line;
+            int contentLength = 0;
+            String contentBody = null;
 
             while ((line = br.readLine()) != null) {
                 if ("".equals(line)) break;
@@ -39,16 +46,96 @@ public class RequestHandler extends Thread {
 
             String requestHeader = sb.toString();
             String startLine = requestHeader.split("\n")[0];
-            String requestURL = HttpRequestUtils.parseRequestUrl(HttpRequestUtils.parseRequestHeaderStartLine(startLine));
+
+            String[] startLineTokens = parseRequestHeaderStartLine(startLine);
+            String requestMethod = startLineTokens[0];
+            String requestURL = startLineTokens[1];
+
+
+//            String requestURL = HttpRequestUtils.parseRequestUrl(HttpRequestUtils.parseRequestHeaderStartLine(startLine));
+//            User user = getUserInfoByGET(requestURL);
+//            if (user != null) {
+//                System.out.println(user.toString());
+//                return;
+//            }
+
+            contentLength = getContentLength(requestHeader);
+            contentBody = IOUtils.readData(br, contentLength);
+
+            if (requestURL.equals("/user/create")) {
+                User user = getCreatedUserInfo(contentBody);
+                DataBase.addUser(user);
+                log.debug("User : {}", user);
+//                requestURL = "/index.html";
+                response302Header(dos);
+            }
+
+
+            if (requestURL.equals("/user/login")) {
+                if (isLoginSuccess(contentBody)){
+                    System.out.println("login success");
+                }
+            }
+
             File requestFile = getFile(getFullFilePath(requestURL));
             if (requestFile.isDirectory()) return;
             byte[] body = Files.readAllBytes(requestFile.toPath());
 
             response200Header(dos, body.length);
             responseBody(dos, body);
+        } catch (
+                IOException e) {
+            log.error(e.getMessage());
+        }
+
+    }
+
+    private void response302Header(DataOutputStream dos) {
+        String location = "/index.html";
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + location);
+            dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+
+    private int getContentLength(String requestHeader) {
+        String[] requestHeaderList = requestHeader.split("\n");
+
+        for (String eachLine : requestHeaderList) {
+            Pair headerMap = parseHeader(eachLine);
+            if (headerMap == null) continue;
+            if (headerMap.getKey().equals("Content-Length")) {
+                return Integer.parseInt(headerMap.getValue());
+            }
+        }
+        return 0;
+    }
+
+    private User getUserInfoByGET(String data) {
+        int index = data.indexOf("?");
+        if (index == -1) return null;
+
+        String requestPath = data.substring(0, index);
+        String params = data.substring(index + 1);
+        return getCreatedUserInfo(params);
+    }
+
+    private User getCreatedUserInfo(String params) {
+        Map<String, String> map = parseQueryString(params);
+        return new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
+    }
+
+    private boolean isLoginSuccess(String params) {
+        Map<String, String> map = parseQueryString(params);
+        String userId = map.get("userId");
+        String password = map.get("password");
+
+        User user = DataBase.findUserById(userId);
+        return user.getPassword().equals(password);
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
